e51d6d584b505ae21df1cb6129a06e178d2a1739
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-04-12 18:55:02 +0530

    frist commit

---
+++ basic-problem/Problem-2.py
@@ -0,0 +1,9 @@
+# Take the person's name and age as input and print out the name and age as specified in the output format.
+
+name = input()
+
+age = int(input())
+
+print("The name of the person is",name,"and the age is",age,end=".")
+
+
