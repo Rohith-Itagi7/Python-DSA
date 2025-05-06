aff16206e6d93ebbd63bca911c41c45dec9ff8aa
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-05-06 23:42:23 +0530

    frist commit

---
+++ While_loop.py
@@ -0,0 +1,38 @@
+count=0
+while count<5:
+    count+=1
+    print(count)
+
+# i=0
+# while i<5:
+#     j=0
+#     while j<5:
+#         print(i,j)
+#         j+=1
+# i+=1
+    
+i=0
+while i<5:
+   i+=1
+   if i==3:
+       break
+   else:
+       print(f"{i}")  
+
+j=0
+while i<5:
+   i+=1
+   if i==3:
+       continue
+   else:
+       print(f"{i}")  
+
+
+j=0
+while i<5:
+   i+=1
+   if i==3:
+       pass
+   else:
+       print(f"{i}")  
+
