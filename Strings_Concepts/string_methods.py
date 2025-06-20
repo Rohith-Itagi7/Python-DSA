8470ec9eed2b467257332774410684aa3f95e2a7
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-06-20 11:08:54 +0530

    converting_methods

---
+++ Strings_Concepts/string_methods.py
@@ -0,0 +1,18 @@
+message="hello World!"
+print(message.strip())#Strip method is used to delete the begin and end spaces of a string.
+
+print(message.split())#split() method is used for spliting string and making a list.
+
+print(message.upper())#upper method is used for  converting the whole string uppercase.
+
+print(message.lower())#lower method is used for converting the whole string to lowercase.
+
+print(message.capitalize())#Which helps to capitalize the frist character of a string.
+
+print(message.title())#title() is udes for converts the first character to upper case and rest to lower case.
+
+print(message.swapcase())#Which converts lowercase letter into uppercase and vice-versa
+
+print(message.center(20,"-")) #the word "hello" is centered within a 20-character wide space
+
+print(message.count("l"))
