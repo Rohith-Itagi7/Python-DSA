3869a685ef4c8153ff96074ef923f5c8231808e9
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2025-07-12 21:57:21 +0530

    Add files via upload

---
+++ Set_Concepts/set_method.py
@@ -0,0 +1,39 @@
+#Clear method
+fruits = {"apple", "banana", "cherry"}
+fruits.clear()
+
+print(fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+print(fruits)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.isdisjoint(y)
+
+print(z)
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+z = x.issubset(y) 
+
+print(z)
+
+
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+print(fruits)
+
+
+fruits = {"apple", "banana", "cherry"}
+fruits.pop()
+
+print(fruits)
+
+x = {"f", "e", "d", "c", "b", "a"}
+y = {"a", "b", "c"}
+z = x.issuperset(y)
+
+print(z)
