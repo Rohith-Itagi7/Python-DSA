8bef0ecef820b8a804771a0a54fede69df4600ec
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2025-07-27 23:20:19 +0530

    Update Python Swapcase.py

--- Python Swapcase.py
+++ Python Swapcase.py
@@ -1,11 +1,11 @@
 #Using Swapcase()
 lower_case="hey how are you"
-case_1=lower_case.swapcase()
+case_1=lower_case.swapcase()#it changes into upper case 
 print(case_1)
 
 Upper_case="HEY HOW ARE YOU"
 case_2=Upper_case.swapcase()
-print(case_2)
+print(case_2)#it changes the string into lower case 
 
 str="HEy HoW ArE yOu"
 
