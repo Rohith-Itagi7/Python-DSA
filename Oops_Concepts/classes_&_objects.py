fc208f218d0f6c96eedcc10b1f48cc921d307e1f
Rohith-Itagi7 <rohititagi2@gamil.com>
2025-09-06 19:27:58 +0530

    initail commit

---
+++ Oops_Concepts/classes_&_objects.py
@@ -0,0 +1,8 @@
+#class and objects are the two core concepts in oops where objects are created based on the class
+class car:    #here car is an class
+    price=2000
+
+total=car()   #total is an object
+print(total.price)
+
+#All class have a _init_method() which is always executed whenever the class is being initiated
