c499cbef0bbd11092bf7c650b92c6846ab1b2790
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2026-04-07 01:07:45 +0530

    Update dictionaries_representation.py

--- dictionaries_Concepts/dictionaries_representation.py
+++ dictionaries_Concepts/dictionaries_representation.py
@@ -1,83 +1,15 @@
-# Dictionary items are ordered, changeable, and do not allow duplicates.
-this_dict={
-    "name":"Rohith",
-    "Birth":1976,
-    "place":"Bangslore"
-}
-print(this_dict)
-
-#getting keys()
-x = this_dict.keys()
-
-#getting values using key 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(thisdict["brand"])
-
-#To get length of the dictionary items
-print(len(thisdict))
-
-# The values in dictionary items can be of any data type:
-thisdict = {
-  "brand": "Ford",
-  "electric": False,
-  "year": 1964,
-  "colors": ["red", "white", "blue"]
+dict={
+    "Name": "Rohith",
+    "Year": 2005,
+    "Birth_Place": "Bangalore",
 }
 
-# Using the dict() method to make a dictionary
-thisdict = dict(name = "John", age = 36, country = "Norway")
-print(thisdict)
+print(dict)
 
-# Add a new item to the original dictionary, and see that the keys list gets updated as well
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
+thisdict={
+    1:"Rohith",
+    2:"Rakshitha"
+    3:"Manu"
+    4:"Venki"
 }
-
-x = car.keys()
-print(x) 
-
-car["color"] = "white"
-print(x)
-
-#Getting values
-x = thisdict.values()
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-
-x = car.values()
-print(x) #before the change
-
-car["year"] = 2020
-print(x) #after the change
-
-# The items() method will return each item in a dictionary, as tuples in a list
-x = thisdict.items()
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-
-x = car.items()
-print(x) #before the change
-car["color"] = "red"
-
-print(x)
-
-# To determine if a specified key is present in a dictionary use the in keyword
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-if "model" in thisdict:
-  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+print(thisdict[1])
