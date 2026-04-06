571df6f258640d2d4c8704702dfb451c96b6668e
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2026-04-06 23:58:43 +0530

    Refactor list operations and examples in Python
    Updated list operations and examples, including adding, accessing, and modifying elements.

--- List_Concepts/list-representation.py
+++ List_Concepts/list-representation.py
@@ -1,68 +1,24 @@
-"""variable naming"""
-#Camel case
-myVariableName=123
-#Pascal case
-MyVariableName=123
-#Snake case
-my_variable_name=123
-list0=["Rohith","Four","Three"]
-list1=[1,2,3,4,5,6]
-list3=[True,False,True,False]
-list_ = ["abc", 34, True, 40, "male"]#list contaning the all different types of data.
-print(type(list1))
+my_list=["Football","Cricket","KHo-Kho","Volleyball"]
+print(my_list)
 
-my=list(("Apple","Orange","Rock"))#Construct list using list()
-print(my)
+Foods=["1",3,True,] #It holds different data types
+print(Foods)
 
-#Accesing list items
-my_list=["Rohith","Four","Three"]
-print(my_list[1])
+Habits=["Walk","Jog","Meditation","Book","Sleeping early"]
+Habits[0]="Dance"  #Replacing the Elements
+print(Habits)
 
-#Accessing list in reverse order 
-my_list=["Rohith","Four","Three"]
-print(my_list[-1])
+#Accesing the list elements
+Cities=["Davngere","Harihar","Bangalore","Shivomoga""Madikeri"]
+print(Cities[0:]) 
+print(Cities[2:4])
+print(Cities[:-1])
+print(Cities[::2])
 
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:5])
-print(thislist[3:6])
-print(thislist[2:])
-print(thislist[:4])
+fav_stars=["Yash","Darshan","Sudeep","yograj"]
+fav_stars.append("Rohith")
+print(fav_stars)
 
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:3] = ["blackcurrant", "watermelon"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:2]=["blackcurrant", "watermelon"]
-print(thislist)
-
-#Adding list elements
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(2, "watermelon")
-print(thislist)
-
-thislist=["apple", "banana", "cherry"]
-thislist.append("Orange")
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-tropical = ["mango", "pineapple", "papaya"]
-thislist.extend(tropical)
-print(thislist)
-
-#Removing the element from the list
-thislist = ["apple", "banana", "cherry"]
-thislist.remove("banana")
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist.pop(2)
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist.pop()#removes the last element
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-del thislist[0] #del function is used to delete list item and even we can delete entier list using del
-print(thislist)
+fav_stars=["Yash","Darshan","Sudeep","yograj"]
+fav_stars.remove("Yash")
+print(fav_stars)
