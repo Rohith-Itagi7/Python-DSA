86c7b8d4825f89883ab16ca9652b79e31dffa1cc
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2026-04-09 15:22:04 +0530

    Update While_loop.py with new examples and fixes
    Refactor while loop examples and correct syntax errors.

--- While_loop.py
+++ While_loop.py
@@ -1,44 +1,35 @@
 #increment using while loop
 count=0
-while count<5:
-    count+=1
-    print(count)
-
-# Nested while loop
-i=0
-while i<5:
-    j=0
-    while j<5:
-        print(i,j)
-        j+=1
-i+=1
-
-#while loop with breal function.here break function exits from code even if the condition is true.
-i=0
-while i<5:
-   i+=1
-   if i==3:
-       break
-   else:
-       print(f"{i}")  
-
+while i <=6:
+    print("Hey")
+    i+=1
+    
+#while loop with break function.here break function exits from code even if the condition is true.
+count=0
+while i <=6:
+    i+=1
+    if i==3:
+        break
+    else:
+        print("Hey")
 #while_loop using continue,here continue skips the current itreation nd move to next itreartion
-i=0
-while i<5:
-   i+=1
-   if i==3:
-       continue
-   else:
-       print(f"{i}")  
+count=1
+while i<=6:
+    i+=1
+    if i==3
+      continue
+    else:
+       print("Hey")
+        
+#pass in while loop ,here pass do nothing when i==3
+count=1
+while i<=6:
+    i+=1
+    if i==3
+      pass
+else:
+    print("Hey")
 
-#pas in while loop ,here pass do nothing when i==3
-j=0
-while i<5:
-   i+=1
-   if i==3:
-       pass
-   else:
-       print("i")  
 #While loop using else block
 i = 0
 while i < 5:
@@ -46,9 +37,4 @@ while i < 5:
     i += 1
 else:
     print("Loop finished without a break.")
-
-#While loop with one condition
-count = 1
-
-while count <= 5: print(count); count += 1
 
