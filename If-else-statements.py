8ef00a5a5662af92ee03a4295ed2e7376b803981
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2026-04-09 14:38:49 +0530

    Update If-else-statements.py

--- If-else-statements.py
+++ If-else-statements.py
@@ -1,46 +1,43 @@
-x=50
-y=60
-if x<y:
-    print("x is less
+#if statement
+score_a=60
+score_b=40
 
-# if-else
-x=70
-y=30
-if x>y:
-    print("x is greater than y")
+if score_a> score_b:
+    print("You have passed")
+
+#if-else statement
+score_a=60
+score_b=40
+
+if score_a> score_b:
+    print("You have passed")
 else:
-    print("X is less than Y")
+    print("You have failed")
 
-#elif ladder
-age=int(input("Enter your age:"))
+#elif ladder with comparision operator
+score_a=60
+score_b=40
 
-if age>18:
-    print("You'r dult now")
-elif age==18:
-    print("Still your a teen age")
-elif age==16:
-    print("You'r minor")
+if score_a >= score_b:
+    print("You have passed")
+elif score_a<score_b:
+    print("You have too wait for result")
+elif score_a <= score_b:
+    print("Vist Examination hall")
 else:
-    print("Still you are child")
-
-# if shorthand
-x=30
-y=20
-if (x>y):print("x is greater than y")
+    print("You have failed")
+    
+#Short-hand if
+a=10
+b=20
+if (a>b): print('a is  not a greater than b')
 
 #if-else shorthand
-x=50
-y=40
-print('x >y' if x<y else('x<y'))
+a=10
+b=20
+print('a>b' if a>b else ('a<b'))    
 
-a = 2
-b = 330
-print("A") if a > b else print("B")
-
-# we can also have multiple else statements on the same line
-a = 330
-b = 330
-print("A") if a > b else print("=") if a == b else print("B")
+print("A") if a<b else print("B")
 
 #Using Logical Operator
 a = 200
@@ -48,7 +45,7 @@ b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
-
+    
 a = 200
 b = 33
 c = 500
@@ -68,5 +65,9 @@ if age<20:
    if age==18:
       print("Average Weight")
    else:
-      print("Perfect weight")
+      if age<=18:
+        print("Perfect weight")
+
+
+
 
