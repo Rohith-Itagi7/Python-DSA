7d4b34df46c0c4f14b02974d5f235f4cf124fbbf
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2026-08-19 23:41:12 +0530

    Add sorting examples for strings and numbers

--- List_Concepts/Sort_Copy_Join_list.py
+++ List_Concepts/Sort_Copy_Join_list.py
@@ -1,7 +1,9 @@
+#sort Alphabetvise
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
 thislist.sort()
 print(thislist)
 
+#Ascending order
 thislist = [100, 50, 65, 82, 23]
 thislist.sort()
 print(thislist)
